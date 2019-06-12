83ed283d69481c8998ca2b349ac33c2c290b22aa
pytoday <jackietsui72@gmail.com>
2019-06-12 10:15:07 +0800

    add init file

---
+++ default_value.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i; float f; double d; char c;
+    cout << "default value of int is:" << i << "!" << endl;
+    cout << "default value of float is:" << f << "!" << endl;
+    cout << "default value of double is:" << d << "!" << endl;
+    cout << "default value of cahr is:" << c << "!" << endl;
+    return 0;
+}
